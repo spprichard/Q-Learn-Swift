abd6224fd263ca38608693e5e988063b25996b8c
Jeffrey Fulton <jeffrey.fulton@me.com>
2017-12-11 21:46:03 -0600

    Added some comments and one line of code to main.swift.

--- Sources/Q-Learn-Swift/main.swift
+++ Sources/Q-Learn-Swift/main.swift
@@ -15,3 +15,8 @@ func main() {
     
     print("Contents: \(contents)")
 }
+
+// This main.swift file IS the script that will execute when this program runs, which is why you're not seeing any output, you have defined a `func main()` but you havn't called it. Try this:
+main()
+
+// Also, check out this blog post, I think you'll like it: https://www.swiftbysundell.com/posts/building-a-command-line-tool-using-the-swift-package-manager
