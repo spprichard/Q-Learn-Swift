51a215cded464b06561e1cdeccb5f5631af321f2
stevenprichard <steven.prichard@boldcommerce.com>
2017-12-14 22:00:48 -0600

    added more parser function

--- Sources/Q-Learn-Swift/Parser.swift
+++ Sources/Q-Learn-Swift/Parser.swift
@@ -15,15 +15,36 @@ struct Parser {
         return self.data.count
     }
     
-    func CountWalls() -> Int {
-        
+    func WallCount() -> Int {
+        var wallCount = 0
         self.data.forEach { item in
-            var wallCount: Int
             if item == "O" {
                 wallCount += 1
             }
         }
         return wallCount
+    }
+    
+    func BoarderCount() -> Int{
+        var boarderCount = 0
+        self.data.forEach { item in
+            if item == "-" {
+                boarderCount += 1
+            }
+        }
+        return boarderCount/2
+    }
+    
+    // TODO: Figure this out
+    func RowCount() -> Int {
+        var itemCount = 0
+        self.data.forEach { item in
+            if item != "-" {
+                itemCount += 1
+            }
+        }
+        print("Total Row Count: \(itemCount)")
+        return itemCount % 20
     }
 }
 
--- Sources/Q-Learn-Swift/main.swift
+++ Sources/Q-Learn-Swift/main.swift
@@ -5,9 +5,10 @@ func main() {
         let environment = try Environment(processInfo: ProcessInfo.processInfo)
         let data = try String(contentsOfFile: environment.path, encoding: .ascii)
         let parser =  Parser(dataToParse: data)
-        print(parser.ShowData())
-        print(parser.Size())
-        print(parser.CountWalls())
+        print("Data: \n\(parser.ShowData())")
+        print("File Size: \(parser.Size())")
+        print("Wall Count: \(parser.WallCount())")
+        print("Boarder Count: \(parser.BoarderCount())")
     } catch {
         print("There was an errrrrrr....with reading from file \n\(error)")
     }
